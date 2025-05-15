f480b77ed4677b3f7a6993996e7629c31d2f5e96
Srushti Sunkad <srushtisunkad.cs23@bmsce.ac.in>
2025-05-15 12:32:03 +0530

    Lab13__ Dijkstra’s algorithm.c

--- Lab13__ Dijkstra’s algorithm.c
+++ Lab13__ Dijkstra’s algorithm.c
@@ -1,77 +1,80 @@
-#include&lt;stdio.h&gt;
-#include&lt;conio.h&gt;
-void main()
-{
-int i,j,n,v,k,min,u,c[20][20],s[20],d[20];
-clrscr();
-printf(&quot;\n Enter the no. of vertices : &quot;);
-scanf(&quot;%d&quot;,&amp;n);
-printf(&quot;\n Enter the cost adjacency matrix : &quot;);
-printf(&quot;\n Enter 999 for no edge &quot;);
-for(i=1;i&lt;=n;i++)
-{
-for(j=1;j&lt;=n;j++)
-{
-scanf(&quot;%d&quot;,&amp;c[i][j]);
-}
-}
-printf(&quot;\n Enter the source vertex : &quot;);
-scanf(&quot;%d&quot;,&amp;v);
-for(i=1;i&lt;=n;i++)
-{
-s[i]=0;
-d[i]=c[v][i];
-}
-d[v]=0;
-s[v]=1;
-for(k=2;k&lt;=n;k++)
-{
-min=999;
-for(i=1;i&lt;=n;i++)
-{
-if((s[i]==0)&amp;&amp;(d[i]&lt;min))
-{
-min=d[i];
-u=i;
-}
-}
-s[u]=1;
-for(i=1;i&lt;=n;i++)
-{
+#include <stdio.h>
+#include <conio.h>
 
-if(s[i]==0)
-{
-if(d[i]&gt;(d[u]+c[u][i]))
-{
-d[i]=d[u]+c[u][i];
+void main() {
+    int i, j, n, v, k, min, u;
+    int c[20][20], s[20], d[20];
+    
+    clrscr();
+    
+    printf("\nEnter the no. of vertices: ");
+    scanf("%d", &n);
+    
+    printf("\nEnter the cost adjacency matrix:\n");
+    printf("Enter 999 for no edge\n");
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            scanf("%d", &c[i][j]);
+        }
+    }
+    
+    printf("\nEnter the source vertex: ");
+    scanf("%d", &v);
+    
+    // Initialize
+    for (i = 1; i <= n; i++) {
+        s[i] = 0;
+        d[i] = c[v][i];
+    }
+    d[v] = 0;
+    s[v] = 1;
+    
+    for (k = 2; k <= n; k++) {
+        min = 999;
+        u = -1;
+        
+        // Find vertex u with minimum distance not in S
+        for (i = 1; i <= n; i++) {
+            if (s[i] == 0 && d[i] < min) {
+                min = d[i];
+                u = i;
+            }
+        }
+        
+        s[u] = 1;
+        
+        // Update distances
+        for (i = 1; i <= n; i++) {
+            if (s[i] == 0 && d[i] > d[u] + c[u][i]) {
+                d[i] = d[u] + c[u][i];
+            }
+        }
+    }
+    
+    printf("\nThe shortest distance from %d is:\n", v);
+    for (i = 1; i <= n; i++) {
+        printf("%d --> %d = %d\n", v, i, d[i]);
+    }
+    
+    getch();
 }
-}
-
-}
-}
-printf(&quot;\n The shortest distance from %d is &quot;,v);
-for(i=1;i&lt;=n;i++)
-{
-printf(&quot;\n %d --&gt; %d = %d &quot;,v,i,d[i]);
-}
-getch();
-}
-
 
 OUTPUT
-
 Enter the no. of vertices: 5
+
 Enter the cost adjacency matrix:
-Enter 999 for no edge:
+Enter 999 for no edge
 999 7 3 999 999
 7 999 2 5 4
 3 2 999 4 999
 999 5 4 999 6
 999 4 999 6 999
+
 Enter the source vertex: 1
+
 The shortest distance from 1 is:
-1 --&gt; 1 = 0
-1 --&gt; 2 = 5
-1 --&gt; 3 = 3
-1 --&gt; 4 = 7
-1 --&gt; 5 = 9
+1 --> 1 = 0
+1 --> 2 = 5
+1 --> 3 = 3
+1 --> 4 = 7
+1 --> 5 = 9
